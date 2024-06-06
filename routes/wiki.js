ef8534433eeb64080ce2308a184c1a8e2638c824
LinKuangren <romaindanne@gmail.com>
2024-06-07 00:57:17 +0200

    Update: wiki.js

--- routes/wiki.js
+++ routes/wiki.js
@@ -9,4 +9,26 @@ router.get('/', async function(req, res, next) {
     res.send(configuration);
 });
 
+router.post('/new-configuration', async function(req, res) {
+    
+})
+
+router.delete('/:id', async function(req, res) {
+    const configuration = await prisma.configuration.delete({
+        where: {
+            id: parseInt(req.params.id)
+        }
+    });
+
+    res.status(200).json({ message: `La configuration est supprimé.` });
+});
+
+router.get('/:id', async function(req, res){
+    
+});
+ 
+router.put("/:id", async function (req, res) {
+    
+});
+
 module.exports = router;
