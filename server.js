d892ea5ce6b38d2f8c8c03de75de5dc10d127249
LinKuangren <romaindanne@gmail.com>
2024-07-11 22:36:00 +0200

    update: oublie fichier

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
@@ -7,6 +8,8 @@ const prisma = new PrismaClient();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const wikiRouter = require('./routes/wiki');
 app.use('/wiki', wikiRouter);
