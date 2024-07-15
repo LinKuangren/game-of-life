c7f6c3d315962b21712c9d57f5c577f4a2f066f0
Yohan Maury <mauryyohan1@gmail.com>
2024-07-15 17:30:31 +0200

    feat: interceptor axios pour faciliter les requetes en integrant, de base, le token dans chaque entete de requete

--- front/src/main.ts
+++ front/src/main.ts
@@ -8,6 +8,16 @@ import App from './App.vue'
 import router from './router'
 import Navigation from './components/Navigation.vue'
 import Footer from './components/Footer.vue'
+import axios from 'axios';
+
+// Configurez l'intercepteur
+axios.interceptors.request.use(config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
 
 const app = createApp(App)
 
