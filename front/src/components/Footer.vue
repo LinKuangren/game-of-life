ece7d693374c0b98196f49948dc2cfb0f0abb083
LinKuangren <romaindanne@gmail.com>
2024-07-18 17:42:40 +0200

    update: ajout du bouton déconnexion

--- front/src/components/Footer.vue
+++ front/src/components/Footer.vue
@@ -5,7 +5,8 @@
         <router-link to="/" class="my-0 mx-2">Accueil</router-link>
         <router-link to="/wiki" class="my-0 mx-2">Wiki</router-link>
         <router-link to="/" class="my-0 mx-2">Simulateur</router-link>
-        <router-link to="/login" class="my-0 mx-2">Se connecter</router-link>
+        <router-link v-if="!isLoggedIn" to="/login" class="my-0 mx-2">Se connecter</router-link>
+        <button v-else @click="logout()" class="my-0 mx-2">Déconnexion</button>
       </nav>
     </div>
     <div class="flex justify-center">
@@ -20,3 +21,30 @@
     </div>
   </footer>
 </template>
+
+<script>
+import { isAuthenticated } from '@/utils/auth.js';
+
+export default {
+  data() {
+    return {
+      isLoggedIn: isAuthenticated()
+    };
+  },
+  created() {
+    this.isLoggedIn = isAuthenticated();
+  },
+  methods: {
+    logout() {
+      localStorage.removeItem('token');
+      this.isLoggedIn = false;
+      this.$router.push('/login');
+    }
+  },
+  watch: {
+    '$route'() {
+      this.isLoggedIn = isAuthenticated();
+    }
+  }
+};
+</script>
